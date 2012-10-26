1e02f5b8bba297112c8f28d99c1f896bc47528ee
Neeraj Singh <neerajdotname@gmail.com>
2012-10-26 19:04:03 -0400

    rake task cleanup

--- lib/p2k/railtie.rb
+++ lib/p2k/railtie.rb
@@ -9,25 +9,16 @@ module P2k
       HPUSHER_URL = 'http://74.207.237.77/heroku'
 
       desc "pushes to heroku"
-      task :push2heroku => :environment do
-        callbacks = (ENV['CALLBACKS'] || '').split(',')
-        config_path = Rails.root.join('config')
-        Base.new(::Job.create!.id, config_path, callbacks).push
-      end
+      task :push2heroku do
+        branch_name = Git.new.current_branch
+        project_name = File.basename(Dir.getwd)
 
-      desc "pushes to heroku via external server"
-      task :push2hpusher, [:project, :branch] do |t, args|
-        unless args[:project] && args[:branch]
-          puts "Usage: rake push2server['tweli','master'] CALLBACKS=reset_db_using_fixtures"
-        end
-
-        response = Net::HTTP.post_form(URI.parse(HPUSHER_URL), { project: args[:project],
-                                                                 branch: args[:branch],
-                                                                 'options[callbacks]' => ENV['CALLBACKS']})
+        response = Net::HTTP.post_form(URI.parse(HPUSHER_URL), { project: project_name, branch: branch_name, 'options[callbacks]' => ENV['CALLBACKS']})
 
         if response.code == '200'
-          puts 'The appliction will be deployed to:'
-          puts MultiJson.load(response.body)['heroku_url']
+          tmp = MultiJson.load(response.body)
+          puts 'The appliction will be deployed to: ' +  tmp['heroku_url']
+          puts 'Monitor the job status at: ' +  tmp['status']
         else
           puts 'Something has gone wrong'
         end
