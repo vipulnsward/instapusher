a276fdb6a7408a480cc710c4b86cd9400ef26840
Neeraj Singh <neerajdotname@gmail.com>
2012-11-07 20:32:10 -0500

    wip

--- lib/instapusher.rb
+++ lib/instapusher.rb
@@ -1,14 +1,8 @@
 require "instapusher/version"
-require "hashr"
 
 module Instapusher
   extend ActiveSupport::Autoload
-
-  autoload :Base
-  autoload :ConfigLoader
   autoload :Git
-  autoload :Util
-  autoload :HerokuAppNameGenerator
 end
 
 require 'instapusher/railtie'
--- lib/instapusher/util.rb
+++
@@ -1,8 +0,0 @@
-class Util
-
-  def self.remote_branch_exists?(remote_branch_name)
-   out = `cd #{Rails.root.expand_path} && git branch -r`
-   out.include?(remote_branch_name)
-  end
-
-end
