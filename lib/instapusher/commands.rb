486782a549ea38817e826a4e66816e99bb9aa10c
Michael Nikitochkin <miry.sof@gmail.com>
2013-03-08 12:13:30 +0200

    Removed LOCAL env.

--- lib/instapusher/commands.rb
+++ lib/instapusher/commands.rb
@@ -7,35 +7,27 @@ module Instapusher
   class Commands
     DEFAULT_HOSTNAME = 'instapusher.com'
 
-    def self.deploy
+    def self.deploy(project_name=nil, branch_name=nil)
 
       hostname = ENV['INSTAPUSHER_HOST'] || DEFAULT_HOSTNAME
 
-      #TODO: Remove this env and use host instead of duplication
-      if ENV['LOCAL']
-        hostname = "localhost:3000"
-      end
-      url = "http://#{hostname}/heroku"
-
-
-      git = Git.new
-      branch_name  = git.current_branch
-      project_name = git.project_name
+      url          = "http://#{hostname}/heroku.json"
+      git          = Git.new
+      branch_name  ||= git.current_branch
+      project_name ||= git.project_name
 
       api_key = Instapusher::Configuration.api_key || ""
       if api_key.to_s.length == 0
         puts "Please enter instapusher api_key at ~/.instapusher "
       end
 
-      options = { project:             project_name,
-                  branch:              branch_name,
-                  local:               ENV['LOCAL'],
-                  api_key:             api_key }
+      options = { project: project_name,
+                  branch:  branch_name,
+                  api_key: api_key }
 
       response = Net::HTTP.post_form URI.parse(url), options
-
-      response_body = MultiJson.load(response.body)
-      job_status_url    = response_body['status'] || response_body['job_status_url']
+      response_body  = MultiJson.load(response.body)
+      job_status_url = response_body['status'] || response_body['job_status_url']
 
       if job_status_url && job_status_url != ""
         puts 'The appliction will be deployed to: ' + response_body['heroku_url']
