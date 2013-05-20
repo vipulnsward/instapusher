9363056bd68d403f371b13ca9f4836c4ae490c33
Neeraj Singh <neerajdotname@gmail.com>
2013-05-20 17:10:46 -0400

    wip

--- lib/instapusher/commands.rb
+++ lib/instapusher/commands.rb
@@ -5,33 +5,55 @@ require 'instapusher'
 
 module Instapusher
   class Commands
+
     DEFAULT_HOSTNAME = 'instapusher.com'
 
-    def self.deploy(_options)
+    attr_reader :debug, :api_key, :branch_name, :project_name
 
-      debug = _options(:debug)
+    def initialize init_options = {}
 
-      hostname = ENV['INSTAPUSHER_HOST'] || DEFAULT_HOSTNAME
-      hostname = "localhost:3000" if _options[:local]
+      @debug = init_options[:debug]
+      @quick = init_options[:quick]
+      @local = init_options[:local]
 
-      url          = "http://#{hostname}/heroku.json"
       git          = Git.new
-      branch_name  = _options[:project_name] || ENV['INSTAPUSHER_BRANCH'] || git.current_branch
-      project_name = _options[:branch_name] || ENV['INSTAPUSHER_PROJECT'] || git.project_name
+      @branch_name  = init_options[:project_name] || ENV['INSTAPUSHER_BRANCH'] || git.current_branch
+      @project_name = init_options[:branch_name] || ENV['INSTAPUSHER_PROJECT'] || git.project_name
+    end
 
-      api_key = ENV['API_KEY'] || Instapusher::Configuration.api_key || ""
+    def options
+      @options ||= begin
+        { project: project_name,
+          branch:  branch_name,
+          quick:   @quick,
+          local:   @local,
+          api_key: api_key }
+      end
+    end
 
-      if api_key.to_s.length == 0
+    def verify_api_key
+      @api_key = ENV['API_KEY'] || Instapusher::Configuration.api_key || ""
+
+      if @api_key.to_s.length == 0
         puts "Please enter instapusher api_key at ~/.instapusher "
       end
+    end
 
-      options = { project: project_name,
-                  branch:  branch_name,
-                  quick:   _options[:quick],
-                  local:   _options[:local],
-                  api_key: api_key }
+    def url
+      @url ||= begin
+          hostname =  if options[:local]
+                        "localhost:3000" 
+                      else
+                        ENV['INSTAPUSHER_HOST'] || DEFAULT_HOSTNAME
+                      end
+          "http://#{hostname}/heroku.json"
+      end
+    end
 
-      if debug || true
+    def deploy
+      verify_api_key
+
+      if debug
         puts "url to hit: #{url.inspect}"
         puts "options being passed to the url: #{options.inspect}"
       end
